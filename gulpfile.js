0214c1fbf31c1ab4026bf88d2b6c846006b7d53a
Vadim-Strakhov <mars1597@gmail.com>
2023-10-01 18:45:16 +0300

    adding gulp, sass and creating header

---
+++ gulpfile.js
@@ -0,0 +1,32 @@
+const gulp = require('gulp');
+const browserSync = require('browser-sync');
+const sass = require('gulp-sass')(require('sass'));
+const autoprefixer = require('gulp-autoprefixer');
+const cleanCss = require('gulp-clean-css');
+
+gulp.task('server', function(){
+
+    browserSync({
+        server: {
+            baseDir: './'
+        }
+    });
+
+    gulp.watch('*.html').on('change', browserSync.reload);
+})
+
+gulp.task('styles', function() {
+    return gulp.src('scss/*.scss')
+        .pipe(sass())
+        .pipe(autoprefixer())
+        .pipe(cleanCss())
+        .pipe(gulp.dest('assets/css/'))
+        .pipe(browserSync.stream());
+})
+
+gulp.task('watch', function() {
+    gulp.watch('scss/*/*.scss', gulp.parallel('styles'));
+    gulp.watch('scss/*.scss', gulp.parallel('styles'));
+})
+
+gulp.task('default', gulp.parallel('watch', 'server', 'styles'));
