3cb0bd23cc12af15e8f8a8bfc3b3852506805451
Vadim-Strakhov <mars1597@gmail.com>
2023-10-11 22:07:42 +0300

    created block new products

--- gulpfile.js
+++ gulpfile.js
@@ -3,34 +3,36 @@ const browserSync = require('browser-sync');
 const sass = require('gulp-sass')(require('sass'));
 const autoprefixer = require('gulp-autoprefixer');
 const cleanCss = require('gulp-clean-css');
-const rename = require("gulp-rename");
+const rename = require('gulp-rename');
 
-gulp.task('server', function(){
-
+gulp.task('server', function () {
     browserSync({
         server: {
-            baseDir: './'
-        }
+            baseDir: './',
+        },
     });
 
     gulp.watch('*.html').on('change', browserSync.reload);
-})
+});
 
-gulp.task('styles', function() {
-    return gulp.src('scss/*.scss')
+gulp.task('styles', function () {
+    return gulp
+        .src('scss/*.scss')
         .pipe(sass())
         .pipe(autoprefixer())
         .pipe(cleanCss())
-        .pipe(rename({
-            suffix: '.min'
-        }))
+        .pipe(
+            rename({
+                suffix: '.min',
+            })
+        )
         .pipe(gulp.dest('assets/css/'))
         .pipe(browserSync.stream());
-})
+});
 
-gulp.task('watch', function() {
+gulp.task('watch', function () {
     gulp.watch('scss/*/*.scss', gulp.parallel('styles'));
     gulp.watch('scss/*.scss', gulp.parallel('styles'));
-})
+});
 
 gulp.task('default', gulp.parallel('watch', 'server', 'styles'));
